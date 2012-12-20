f574c0299464eeb4b27d5102c3af73974273a30c
Project OverClock <projectoverclock@gmail.com>
2012-12-19 21:41:51 -0700

    action script in progress

--- Project Overclock/Assets/Game scripts/PickUpAction.js
+++ Project Overclock/Assets/Game scripts/PickUpAction.js
@@ -13,6 +13,17 @@ function Start () {
 
 function Update () {
 	locationPointOfInterest = pointOfIntrest.transform.position;
-	distance = pointOfIntrest.transform.position - transform.position;
+	
+	if (player.GetDistance(
+	
 	print(distance.x + ", " + distance.y + ", " + distance.z);
+}
+
+function GetDistance(pos1:GameObject, pos2:GameObject)
+{
+	var pos1Loc:Vector3 = pos1.transform.position;
+	var pos2Loc:Vector3 = pos2.transform.position;
+	var distance:Vector3 = pos2Loc - pos1Loc;
+	
+	return distance;
 }
